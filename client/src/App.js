dff5f07949082cb068c5df544734e3b5ebce0f18
vsreyes <virgilio.s.reyes@gmail.com>
2020-10-18 13:06:23 +0200

    Edited App

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 
-const App = () =>  (
-    <div className="App">
-      
+const App = () => ( 
+    <div className='container'>
+        <h4 className="display-4 text.center mb-4">
+          <i className="fab fa-react" /> React File Upload
+        </h4>    
     </div>
-  );
-
+);
 
 export default App;
